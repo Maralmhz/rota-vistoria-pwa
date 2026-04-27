0ab47be0527c8a23e15a7c708a9f5730b3be0691
Maralmhz <maralmhz@gmail.com>
2026-04-27 17:30:30 -0300

    fix: base path dinamico — GitHub Pages e Vercel

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/rota-vistoria-pwa/',
+  base: process.env.GITHUB_ACTIONS ? '/rota-vistoria-pwa/' : '/',
   plugins: [
     react(),
   ],
